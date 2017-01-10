adf65db7f4608f10de2b253fe95b30d0203e83b6
Antoine COTINAT <antoine.cotinat@gmail.com>
2017-01-10 22:13:45 +0100

    Suppression du menu simple (en haut à droite)

--- AndroidStudioProjects/app/src/main/java/fr/ensicaen/projetintensif/MainActivity.java
+++ AndroidStudioProjects/app/src/main/java/fr/ensicaen/projetintensif/MainActivity.java
@@ -3,9 +3,8 @@ package fr.ensicaen.projetintensif;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,13 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
 
-import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
 
 
 public class MainActivity extends AppCompatActivity
@@ -74,7 +69,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
