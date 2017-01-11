f15aea426e14f6fbeee15ec131f577587144b15d
Antoine COTINAT <antoine.cotinat@gmail.com>
2017-01-11 12:26:37 +0100

    Modification MapManager, préparation pour récupération position

--- AndroidStudioProjects/app/src/main/java/fr/ensicaen/projetintensif/MainActivity.java
+++ AndroidStudioProjects/app/src/main/java/fr/ensicaen/projetintensif/MainActivity.java
@@ -5,10 +5,7 @@ import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,7 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import org.osmdroid.config.Configuration;
 
@@ -50,7 +46,6 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         MapManager mapManager = new MapManager(this, getApplicationContext());
-        mapManager.init();
     }
 
     @Override
--- AndroidStudioProjects/app/src/main/java/fr/ensicaen/projetintensif/MapManager.java
+++ AndroidStudioProjects/app/src/main/java/fr/ensicaen/projetintensif/MapManager.java
@@ -2,6 +2,7 @@ package fr.ensicaen.projetintensif;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Location;
 import android.preference.PreferenceManager;
 
 import org.osmdroid.api.IMapController;
@@ -9,30 +10,34 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 
 public class MapManager {
-    Activity _activity;
-    Context _ctx;
+    private Activity _activity;
+    private Context _ctx;
 
     public MapManager(Activity activity, Context ctx) {
         _activity = activity;
         _ctx = ctx;
+
+        init();
     }
 
-    public void init()
+    private void init()
     {
         Configuration.getInstance().load(_ctx, PreferenceManager.getDefaultSharedPreferences(_ctx));
 
         MapView map = (MapView) _activity.findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
-        map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
-
+/*
+        GpsMyLocationProvider gpsMyLocationProvider = new GpsMyLocationProvider(_ctx);
+        Location lastLocation = gpsMyLocationProvider.getLastKnownLocation();
+*/
         IMapController mapController = map.getController();
         mapController.setZoom(9);
+        //GeoPoint startPoint = new GeoPoint(lastLocation.getLatitude(), lastLocation.getLongitude());
         GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
         mapController.setCenter(startPoint);
     }
-
-    
 }
