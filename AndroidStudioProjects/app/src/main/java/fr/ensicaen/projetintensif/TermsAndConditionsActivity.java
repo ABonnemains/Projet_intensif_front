97bc44616357eaf5a7efd252ac1cc7eb7d69e8e7
Harrachi <amineharrachi@gmail.com>
2017-01-11 12:09:14 +0100

    conditions generales d'utilisation semi résolues

---
+++ AndroidStudioProjects/app/src/main/java/fr/ensicaen/projetintensif/TermsAndConditionsActivity.java
@@ -0,0 +1,17 @@
+package fr.ensicaen.projetintensif;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by Amine on 11/01/2017.
+ */
+
+public class TermsAndConditionsActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_and_conditions);
+    }
+}
