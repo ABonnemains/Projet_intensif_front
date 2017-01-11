657867a955b3386f6aa28273b005f350b0de85af
Harrachi <amineharrachi@gmail.com>
2017-01-11 18:13:13 +0100

    zzzz

--- AndroidStudioProjects/app/src/main/java/fr/ensicaen/projetintensif/MainActivity.java
+++ AndroidStudioProjects/app/src/main/java/fr/ensicaen/projetintensif/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity
             nickname_view.setText(nickname);
 
         MapManager mapManager = new MapManager(this, getApplicationContext());
-        
+
     }
 
     @Override
