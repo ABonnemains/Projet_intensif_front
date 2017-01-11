9a2fa09a6bb334b8cfcfa325ec95f4a7c9cbdf3d
Antoine COTINAT <antoine.cotinat@gmail.com>
2017-01-11 13:26:18 +0100

    Carte centrée sur la dernière position connue de l'utilisateur

--- AndroidStudioProjects/app/src/main/java/fr/ensicaen/projetintensif/MapManager.java
+++ AndroidStudioProjects/app/src/main/java/fr/ensicaen/projetintensif/MapManager.java
@@ -2,8 +2,11 @@ package fr.ensicaen.projetintensif;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.preference.PreferenceManager;
+import android.support.v4.content.ContextCompat;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
@@ -12,9 +15,12 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 
+import java.util.List;
+
 public class MapManager {
     private Activity _activity;
     private Context _ctx;
+    private Location _currentLocation;
 
     public MapManager(Activity activity, Context ctx) {
         _activity = activity;
@@ -30,14 +36,46 @@ public class MapManager {
         MapView map = (MapView) _activity.findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setMultiTouchControls(true);
-/*
-        GpsMyLocationProvider gpsMyLocationProvider = new GpsMyLocationProvider(_ctx);
-        Location lastLocation = gpsMyLocationProvider.getLastKnownLocation();
-*/
+
+        Location currentLocation = getLocation();
+        if (currentLocation != null) {
+            _currentLocation = currentLocation;
+        }
+        else {
+            currentLocation.setLongitude(48.8583);
+            currentLocation.setLatitude(2.2944);
+        }
+
         IMapController mapController = map.getController();
-        mapController.setZoom(9);
-        //GeoPoint startPoint = new GeoPoint(lastLocation.getLatitude(), lastLocation.getLongitude());
-        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        mapController.setZoom(14);
+
+        GeoPoint startPoint = new GeoPoint(currentLocation.getLatitude(), currentLocation.getLongitude());
         mapController.setCenter(startPoint);
+    }
+
+    private Location getLocation()
+    {
+        if(ContextCompat.checkSelfPermission( _ctx, android.Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_DENIED &&
+                ContextCompat.checkSelfPermission( _ctx, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_DENIED) {
+            return null;
+        }
+
+        LocationManager locationManager= (LocationManager) _ctx.getSystemService(Context.LOCATION_SERVICE);
+        List<String> providers = locationManager.getProviders(true);
+        Location bestLocation = null;
+
+        for (String provider : providers) {
+            Location location = locationManager.getLastKnownLocation(provider);
+
+            if (location == null) {
+                continue;
+            }
+
+            if (bestLocation == null || location.getAccuracy() < bestLocation.getAccuracy()) {
+                bestLocation = location;
+            }
+        }
+
+        return bestLocation;
     }
 }
